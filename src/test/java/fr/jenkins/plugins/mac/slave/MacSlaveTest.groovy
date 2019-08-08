9ac7f252871d64ba0192c512aa6f6bc0eb91b06d
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2019-08-08 11:44:36 +0200

    tests MacSlave

--- src/test/java/fr/jenkins/plugins/mac/slave/MacSlaveTest.groovy
+++ src/test/java/fr/jenkins/plugins/mac/slave/MacSlaveTest.groovy
@@ -5,6 +5,7 @@ import fr.jenkins.plugins.mac.MacHost
 import fr.jenkins.plugins.mac.MacUser
 import fr.jenkins.plugins.mac.connector.MacComputerJNLPConnector
 import fr.jenkins.plugins.mac.test.builders.MacPojoBuilder
+import hudson.model.TaskListener
 
 import org.junit.Rule
 import org.jvnet.hudson.test.JenkinsRule
@@ -12,10 +13,10 @@ import org.jvnet.hudson.test.JenkinsRule
 import spock.lang.Specification
 
 class MacSlaveTest extends Specification {
-    //TODO : MacSlaveTest
+
     @Rule
     JenkinsRule jenkinsRule
-    
+
     def "should create slave"() {
         setup:
         List<MacHost> macHosts = MacPojoBuilder.buildMacHost()
@@ -23,12 +24,70 @@ class MacSlaveTest extends Specification {
         MacUser user = MacPojoBuilder.buildUser()
         MacCloud cloud = new MacCloud("test", macHosts, connector, "testLabel", false)
         MacSlave slave = new MacSlave("test","testLabel", user, macHosts.get(0), connector.createLauncher(macHosts.get(0), user))
-        
+
         when:
+        jenkinsRule.jenkins.get().clouds.add(cloud)
         jenkinsRule.jenkins.get().addNode(slave)
-        
+
         then:
         notThrown Exception
         jenkinsRule.jenkins.get().getNode(slave.name) == slave
+    }
+
+    def "should terminate and remove slave"() {
+        setup:
+        List<MacHost> macHosts = MacPojoBuilder.buildMacHost()
+        MacComputerJNLPConnector connector = MacPojoBuilder.buildConnector(jenkinsRule)
+        MacUser user = MacPojoBuilder.buildUser()
+        MacCloud cloud = new MacCloud("test", macHosts, connector, "testLabel", false)
+        MacSlave slave = new MacSlave("test","testLabel", user, macHosts.get(0), connector.createLauncher(macHosts.get(0), user))
+
+        when:
+        jenkinsRule.jenkins.get().clouds.add(cloud)
+        jenkinsRule.jenkins.get().addNode(slave)
+        assert jenkinsRule.jenkins.get().getNode(slave.name) == slave
+        slave._terminate(TaskListener.NULL)
+
+        then:
+        notThrown Exception
+        jenkinsRule.jenkins.get().getNode(slave.name) == null
+    }
+
+    def "should return the mac cloud of the slave"() {
+        setup:
+        List<MacHost> macHosts = MacPojoBuilder.buildMacHost()
+        MacComputerJNLPConnector connector = MacPojoBuilder.buildConnector(jenkinsRule)
+        MacUser user = MacPojoBuilder.buildUser()
+        MacCloud cloud = new MacCloud("test", macHosts, connector, "testLabel", false)
+        MacSlave slave = new MacSlave("test","testLabel", user, macHosts.get(0), connector.createLauncher(macHosts.get(0), user))
+
+        when:
+        jenkinsRule.jenkins.get().clouds.add(cloud)
+        jenkinsRule.jenkins.get().addNode(slave)
+        assert jenkinsRule.jenkins.get().getNode(slave.name) == slave
+        MacCloud result = slave.getCloud()
+        
+        then:
+        notThrown Exception
+        result == cloud
+    }
+
+    def "should return node name on mac cloud"() {
+        setup:
+        List<MacHost> macHosts = MacPojoBuilder.buildMacHost()
+        MacComputerJNLPConnector connector = MacPojoBuilder.buildConnector(jenkinsRule)
+        MacUser user = MacPojoBuilder.buildUser()
+        MacCloud cloud = new MacCloud("test", macHosts, connector, "testLabel", false)
+        MacSlave slave = new MacSlave("test","testLabel", user, macHosts.get(0), connector.createLauncher(macHosts.get(0), user))
+
+        when:
+        jenkinsRule.jenkins.get().clouds.add(cloud)
+        jenkinsRule.jenkins.get().addNode(slave)
+        assert jenkinsRule.jenkins.get().getNode(slave.name) == slave
+        String result = slave.displayName
+
+        then:
+        notThrown Exception
+        result == user.username + " on " + cloud.name
     }
 }
