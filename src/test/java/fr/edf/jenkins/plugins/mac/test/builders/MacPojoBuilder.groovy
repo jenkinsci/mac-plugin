4da5a2322b19d5eae58b0d2131d46d9937bcdebd
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2019-12-06 16:30:48 +0100

    update tests

--- src/test/java/fr/edf/jenkins/plugins/mac/test/builders/MacPojoBuilder.groovy
+++ src/test/java/fr/edf/jenkins/plugins/mac/test/builders/MacPojoBuilder.groovy
@@ -2,6 +2,7 @@ package fr.edf.jenkins.plugins.mac.test.builders
 
 import org.jvnet.hudson.test.JenkinsRule
 
+import fr.edf.jenkins.plugins.mac.MacEnvVar
 import fr.edf.jenkins.plugins.mac.MacHost
 import fr.edf.jenkins.plugins.mac.MacUser
 import fr.edf.jenkins.plugins.mac.connector.MacComputerConnector
@@ -9,7 +10,7 @@ import fr.edf.jenkins.plugins.mac.connector.MacComputerJNLPConnector
 import hudson.util.Secret
 
 class MacPojoBuilder {
-    
+
     static List<MacHost> buildMacHost() {
         MacHost host = new MacHost(
                 'localhost', //host
@@ -21,7 +22,9 @@ class MacPojoBuilder {
                 5,//kexTimeout
                 false, //disabled
                 5, //maxTries
-                "testLabel" //label
+                "testLabel", //label
+                15, //agentConnectionTimeout
+                buildEnvVars() //envVars
                 )
         List<MacHost> hostList = new ArrayList()
         hostList.add(host)
@@ -31,8 +34,15 @@ class MacPojoBuilder {
     static MacComputerConnector buildConnector(JenkinsRule jenkinsRule) {
         return new MacComputerJNLPConnector(jenkinsRule.getURL().toString())
     }
-    
+
     static MacUser buildUser() {
         return new MacUser("toto", Secret.fromString("root"), "/Users/toto")
+    }
+    
+    static List<MacEnvVar> buildEnvVars() {
+        List<MacEnvVar> envVars = new ArrayList()
+        envVars.add(new MacEnvVar("TEST1", "test1"))
+        envVars.add(new MacEnvVar("TEST2", "test2"))
+        return envVars
     }
 }
