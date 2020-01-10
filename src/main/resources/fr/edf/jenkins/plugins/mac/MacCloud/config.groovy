86f373169897cdc3777e4b358115999cd6983de2
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-01-10 18:37:21 +0100

    add advanced for properties of MacCloud

--- src/main/resources/fr/edf/jenkins/plugins/mac/MacCloud/config.groovy
+++ src/main/resources/fr/edf/jenkins/plugins/mac/MacCloud/config.groovy
@@ -8,20 +8,24 @@ f.entry(title: Messages.Cloud_Name(), field:'name') {
     f.textbox(default:'mac')
 }
 
-f.entry(title:Messages.Host_Title()) {
-    f.repeatableHeteroProperty(
-            field:'macHosts',
-            hasHeader: 'true',
-            addCaption: Messages.Host_Add(),
-            deleteCaption: Messages.Host_Delete(),
-            oneEach:'false',
-            repeatableDeleteButton:'true'
-            )
+f.advanced(title:Messages.Host_Title()) {
+    f.entry(title:Messages.Host_Title()) {
+        f.repeatableHeteroProperty(
+                field:'macHosts',
+                hasHeader: 'true',
+                addCaption: Messages.Host_Add(),
+                deleteCaption: Messages.Host_Delete(),
+                oneEach:'false',
+                repeatableDeleteButton:'true'
+                )
+    }
 }
 
-f.section(title:Messages.Cloud_AgentsProperties()) {
-    f.entry(title:Messages.Cloud_IdleMinutes(), field:'idleMinutes') {
-        f.number(clazz: 'required', min: 1, default: 1)
+f.advanced(title:Messages.Cloud_AgentsProperties()) {
+    f.section(title:Messages.Cloud_AgentsProperties()) {
+        f.entry(title:Messages.Cloud_IdleMinutes(), field:'idleMinutes') {
+            f.number(clazz: 'required', min: 1, default: 1)
+        }
+        f.dropdownDescriptorSelector(title:'Connect method', field:'connector')
     }
-    f.dropdownDescriptorSelector(title:'Connect method', field:'connector')
 }
